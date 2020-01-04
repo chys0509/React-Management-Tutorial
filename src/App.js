9fbd4382753d65461cce9b494478905e528e100a
chys0509 <dessll2@naver.com>
2020-01-04 18:00:44 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div className="lightslategray-background">
+   <img src={logo} alt="logo"/>
+   <h2>Let's develop management sytstem!</h2>
+
+
+  </div>
+
+
+  );
+}
+
+export default App;
